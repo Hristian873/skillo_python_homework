eede44ad8c5ed1fc60bbe27c61a9eeb9421358a3
Hristian <Hristian873@gmail.com>
2024-07-21 14:12:11 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,53 @@
-# This is a sample Python script.
+#task 1
+print("Hello world")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#task 2
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+color = input("Enter your color")
+print (f"My name is {name}, I am {age} years old, and my favorite color is {color}")
 
+#task 3
+x=int(input("Enter length: "))
+y =int(input("Enter width: "))
+sum = 2*x +2*y
+print(sum)
+#task 4
+Temp=int(input("Enter degrees in Farenheit: "))
+Cels = (Temp-32) * (5/9)
+#task 5
+print("Enter two numbers: ")
+x= int(input("First number: "))
+y =int(input("Second number: "))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print(x+y,x-y,x*y,x/y)
 
+#task 6
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+import math
+r=int(input("Enter radius: "))
+area = math.pi * r*r
+circ = 2*math.pi*r
+print(area,circ)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# task 7
+numb=int(input("Enter number: "))
+if numb%2==0:
+    print("The number is even")
+else :
+    print("The number is odd")
+#task 8
+vote_check=int(input("Enter your age: "))
+if vote_check<18:
+    print("You are not eligible to vote")
+else:
+    print("You are eligible to vote")
+#task 9
+some_string=input("Enter a sentence:")
+print (len(some_string))
+
+#task 10
+str1=input("Enter first sentence ")
+str2=input("Enter second sentence ")
+str3 = str1 + str2
+print (str3)
