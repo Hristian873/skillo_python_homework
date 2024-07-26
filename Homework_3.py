f1e33723a6c4b7696da96f8a7c9920804f138ad6
Hristian <Hristian873@gmail.com>
2024-07-27 00:33:19 +0300

    Initial commit

--- Homework_3.py
+++ Homework_3.py
@@ -5,3 +5,22 @@
 # else :
 #     print("The number is odd")
 #Problem 1
+a=0
+b=0
+for Numbers in range(0,101):
+
+   if a%2 ==0:
+       b=a+b
+       print(a)
+
+   a += 1
+
+print(b)
+#problem 2
+answer=int(input("Enter how much is 5 + 17: "))
+while answer != 22:
+    print("Wrong answer try again: ")
+    answer=int(input("Enter how much is 5 + 17: "))
+print("Good job right answer")
+
+#problem 3
